5798dab84e4cac09d89680fe68d6502120ac1d5e
Nemanja Blagojevic <blagojevicnemanja14@yahoo.com>
2018-09-17 16:40:38 +0200

    init

---
+++ github.js
@@ -0,0 +1,23 @@
+class Github {
+  constructor() {
+    this.client_id = 'ceb03c65191a1554a461';
+    this.client_secret = 'c25b589a67f3db5d54dac14fb757325f9b70801e';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user){
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
